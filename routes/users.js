1de845e839cc18759214fd8cda7bc699d27139bd
pigflymoon <pplflymoon2015@gmail.com>
2016-11-25 14:44:34 +1300

    #render successfully

--- routes/users.js
+++ routes/users.js
@@ -2,19 +2,6 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-
-var options = {
-    provider: 'google',
-
-    // Optional depending on the providers
-    httpAdapter: 'https', // Default
-    //AIzaSyAcnccCGHGJkq3VF6SWZNwyPV7mov8YRMU
-    apiKey: 'AIzaSyAcnccCGHGJkq3VF6SWZNwyPV7mov8YRMU',
-    // apiKey: 'AIzaSyAmETJsBnNIgRl9y36Lk7Av-zEUPOygdVw', // for Mapquest, OpenCage, Google Premier
-    formatter: null         // 'gpx', 'string', ...
-};
-
-
 var getLatlng = function () {
     var promise = new Promise(function (resolve, reject) {
         setTimeout(function () {
@@ -52,117 +39,8 @@ var getLatlng = function () {
     return promise;
 };
 
-
-var getAddress = function (allLnglat) {
-
-
-    var promise = new Promise(function (resolve, reject) {
-        var allLocation = [];
-        // setTimeout(function () {
-        for (let lnglat of allLnglat) {
-            var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lnglat + "&location_type=ROOFTOP&result_type=street_address&key=AIzaSyAcnccCGHGJkq3VF6SWZNwyPV7mov8YRMU";
-            //
-            request({
-                url: url,
-                json: true
-            }, function (error, response, data) {
-
-                if (data.status == 'OK') {
-                    var address = data.results[0].formatted_address;
-                    // console.log(address);
-                    allLocation.push(address);
-
-                }
-
-            });
-
-        }
-
-
-        // }, 100);
-        console.log(allLocation);
-        resolve(allLocation);
-    });
-
-    return promise;
-
-};
-var getAllAddress = function (address) {
-    var promise = new Promise(function (resolve, reject) {
-        setTimeout(function () {
-            console.log(address);
-            resolve(address);
-        }, 100);
-    });
-
-    return promise;
-
-};
-
-
-
-var allLnglat =
-    [
-    [-43.17654, 172.65146],
-    [-44.827023, 169.01756],
-    [-38.23555, 176.50584]
-];
-
-
-var promiseArr = [];
-function asyncMethod(lnglat) {
-    var address;
-    return new Promise(function(resolve, reject) {
-        setTimeout(function () {
-            var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lnglat + "&location_type=ROOFTOP&result_type=street_address&key=AIzaSyAcnccCGHGJkq3VF6SWZNwyPV7mov8YRMU";
-            //
-            request({
-                url: url,
-                json: true
-            }, function (error, response, data) {
-
-                if (data.status == 'OK') {
-                    address = data.results[0].formatted_address;
-
-                    resolve([address]);
-                }
-
-            });
-
-        }, 1000);
-
-        // resolve([address]);
-
-    });
-}
-
-for(var i = 0; i < allLnglat.length; i++) {
-    promiseArr.push(asyncMethod(allLnglat[i]));
-}
-
-//
-
-
 router.get('/', function (req, res, next) {
-    // var promiseArr = [];
-    // getLatlng().then(function(allLnglat){
-    //
-    //     for(var i = 0; i < allLnglat.length; i++) {
-    //         promiseArr.push(asyncMethod(allLnglat[i]));
-    //     }
-    //
-    // });
 
-    // Promise.all(promiseArr).then(function(results) {
-    //     console.log("address is : "+results);
-    //     res.render('users', { title: results});
-    //
-    // });
-    // var allLnglat = [
-    //     [-43.17654, 172.65146],
-    //     [-44.827023, 169.01756],
-    //     [-38.23555, 176.50584]
-    // ];
     var allAddress = [];
     getLatlng().then(function(allLnglat){
         allLnglat.reduce(function (p,lnglat) {
@@ -181,7 +59,6 @@ router.get('/', function (req, res, next) {
                             var address = data.results[0].formatted_address;
 
                             allAddress.push(address);
-                            // resolve(allAddress)
                         }
                         resolve(allAddress)
 
@@ -193,10 +70,6 @@ router.get('/', function (req, res, next) {
             });
         }, Promise.resolve()).then(function(result){
             res.render('users', { title: result});
-
-            // console.log(result)
-
-
         });
 
     });
