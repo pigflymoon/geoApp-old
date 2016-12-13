298c162f397810dae5815ef281706c0a134701dc
pigflymoon <pplflymoon2015@gmail.com>
2016-12-13 14:55:48 +1300

    #include google map

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,8 +1,11 @@
 var map;
+var markerIcon = './images/markericon.png';
+
 function initMap() {
+    var myLatlng = new google.maps.LatLng(-41.24447674404542, 174.7618546);
     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 2,
-        center: new google.maps.LatLng(2.8, -187.3),
+        zoom: 5,
+        center: myLatlng,
         mapTypeId: 'terrain'
     });
 
@@ -10,7 +13,8 @@ function initMap() {
     var script = document.createElement('script');
     // This example uses a local copy of the GeoJSON stored at
     // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-    script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+    // script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+    script.src = 'https://json2jsonp.com/?url=https://api.geonet.org.nz/intensity?type=measured&callback=eqfeed_callback';
     document.getElementsByTagName('head')[0].appendChild(script);
 }
 
@@ -22,7 +26,8 @@ window.eqfeed_callback = function (results) {
         var latLng = new google.maps.LatLng(coords[1], coords[0]);
         var marker = new google.maps.Marker({
             position: latLng,
-            map: map
+            map: map,
+            icon: markerIcon
         });
     }
 }
